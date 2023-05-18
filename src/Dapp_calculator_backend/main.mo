2e6c2e9073532f35812d1cc113ecafa92f315e59
Baltazar Galvan Villalobos <bgalvan@Baltazars-MacBook-Pro.local>
2023-05-18 09:25:56 -0600

    Fucntional Calculator commit

--- src/Dapp_calculator_backend/main.mo
+++ src/Dapp_calculator_backend/main.mo
@@ -1,5 +1,75 @@
+import Float "mo:base/Float";
+import Bool "mo:base/Bool";
+
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+    
+    var counter : Float = 0;
+    let sqrtRoot : Float = 0.5;
+
+    private func privatePower (x: Float, y: Float) : Float{
+        return Float.pow(x, y);
+    };
+
+    public func add (x : Float) : async Float{
+        if(Float.isNaN(x)){
+            return counter;  
+        };
+        counter := Float.add(counter,x);
+        return counter;
+    };
+
+    public func sub (x : Float) : async Float{
+        if(Float.isNaN(x)){
+            return counter;  
+        };
+        counter := Float.sub(counter,x);
+        return counter;
+    };
+
+    public func mul (x : Float) : async Float{
+        if(Float.isNaN(x)){
+            return counter;  
+        };
+        counter := Float.mul(counter,x);
+        return counter;
+    };
+
+    public func div (x : Float) : async Float{
+        if(Float.isNaN(x)){
+            return counter;  
+        };
+        if (x!=0){
+            counter := Float.div(counter,x);
+        };
+        return counter;
+    };
+
+    public func reset () : async Float{
+        counter := 0;
+        return counter;
+    };
+
+    public query func see () : async Float{
+        return counter;
+    };
+
+    public func power (x : Float) : async Float{
+        if(Float.isNaN(x)){
+            return counter;  
+        };
+        counter := privatePower(counter,x);
+        return counter;
+    };
+
+    public func sqrt () : async Float{
+        counter := privatePower(counter, sqrtRoot);
+        return counter;
+    };
+
+    public func floor () : async Float{
+        counter := Float.floor(counter);
+        return counter;
+    };
+    
 };
--- src/Dapp_calculator_frontend/src/index.js
+++ src/Dapp_calculator_frontend/src/index.js
@@ -1,19 +1,281 @@
 import { Dapp_calculator_backend } from "../../declarations/Dapp_calculator_backend";
+import { AuthClient } from "@dfinity/auth-client";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const result = document.querySelector("#result");
 
-  const name = document.getElementById("name").value.toString();
+const btns = document.querySelector(".btns");
 
-  button.setAttribute("disabled", true);
+const motokoLogo = document.querySelector(".motoko");
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await Dapp_calculator_backend.greet(name);
+const idValues = [
+  "C",
+  "-1",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
 
-  button.removeAttribute("disabled");
+const buttonsIds = [
+  "clear",
+  "negativo",
+  "porcentaje",
+  "div",
+  "seven",
+  "eight",
+  "nine",
+  "multiply",
+  "four",
+  "five",
+  "six",
+  "minus",
+  "one",
+  "two",
+  "three",
+  "plus",
+  "cero",
+  "point",
+  "iqual",
+];
 
-  document.getElementById("greeting").innerText = greeting;
+let callingBackend = false;
+
+let lastAritmeticOperator = "";
+
+let justMadeIqual = false;
+
+let putToZero = false;
+
+const setDisabled = function (disabled) {
+  const childBtns = btns.children;
+  callingBackend = disabled;
+  for (const btn of childBtns) {
+    if (disabled) {
+      btn.setAttribute("disabled", true);
+      btn.classList.add("callingBackend");
+    }
+    if (!disabled) {
+      btn.removeAttribute("disabled");
+      btn.classList.remove("callingBackend");
+    }
+  }
+  if (disabled) {
+    result.classList.add("callingBackend");
+    motokoLogo.classList.add("motoko_loader");
+  }
+  if (!disabled) {
+    result.classList.remove("callingBackend");
+    motokoLogo.classList.remove("motoko_loader");
+  }
+};
+
+const reset = async function () {
+  setDisabled(true);
+
+  callingBackend = false;
+  lastAritmeticOperator = "";
+  justMadeIqual = false;
+  putToZero = false;
+
+  result.textContent = await Dapp_calculator_backend.reset();
+  setDisabled(false);
+};
+
+reset();
+
+const add = async function (value) {
+  setDisabled(true);
+  result.textContent = await Dapp_calculator_backend.add(value);
+  setDisabled(false);
+  adjustResultSize();
+};
+
+const sub = async function (value) {
+  setDisabled(true);
+  result.textContent = await Dapp_calculator_backend.sub(value);
+  setDisabled(false);
+  adjustResultSize();
+};
+
+const mul = async function (value) {
+  setDisabled(true);
+  result.textContent = await Dapp_calculator_backend.mul(value);
+  setDisabled(false);
+  adjustResultSize();
+};
+
+const div = async function (value) {
+  setDisabled(true);
+  result.textContent = await Dapp_calculator_backend.div(value);
+  setDisabled(false);
+  adjustResultSize();
+};
+
+//Permite revisar que la tecla apretada sea una tecla valida
+const valInt = function (key) {
+  const evalNum = /[0-9.]/;
+  const evalOperator = /[=*+%-/]/;
+  const evalClear = /[Cc]/;
+
+  if (key === "negativo") return "negativo";
+  if (key.match(evalNum) !== null) return "N";
+  if (key.match(evalOperator) !== null) return "O";
+  if (key.match(evalClear) !== null) return "C";
+
+  if (key === "Backspace") return "Backspace";
 
   return false;
+};
+
+//Ajusta el fontsize dependiendo de la longitud del numero
+const adjustResultSize = function () {
+  const longitud = result.textContent.length;
+  let fontSize = "50px";
+  if (longitud < 8) fontSize = "50px";
+  if (longitud > 7 && longitud < 14) fontSize = "30px";
+  if (longitud > 13 && longitud < 21) fontSize = "20px";
+  if (longitud > 20 && longitud < 42) fontSize = "10px";
+  if (longitud > 41) fontSize = "8px";
+  result.style.fontSize = fontSize;
+};
+
+window.addEventListener("keydown", async (e) => {
+  if (callingBackend) return;
+
+  const key = e.key === "Enter" ? "=" : e.key;
+  const validated = valInt(key);
+
+  if (key === "." && result.textContent.indexOf(".") !== -1) return;
+
+  if (!validated) return;
+
+  if (key === "Backspace") {
+    if (result.textContent === "0") return;
+    result.textContent = result.textContent.slice(0, -1);
+
+    if (result.textContent.length === 0) {
+      result.textContent = "0";
+    }
+    adjustResultSize();
+    return;
+  }
+  let index = idValues.findIndex((el) => el === key.toUpperCase());
+  if (key === "Clear") index = 0;
+  const btnClicked = document.querySelector(`#${buttonsIds[index]}`);
+
+  btnClicked.classList.add("clicked");
+
+  setTimeout(() => {
+    btnClicked.classList.remove("clicked");
+  }, 333);
+
+  btnClicked.click();
 });
+
+btns.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const pressedBtn = e.target;
+  if (pressedBtn.id === "negativo") {
+    if (result.textContent.indexOf("-") === -1) {
+      result.textContent = "-" + result.textContent;
+    } else {
+      result.textContent = result.textContent.slice(
+        1,
+        result.textContent.length
+      );
+    }
+    if (justMadeIqual) mul(-1);
+    adjustResultSize();
+    return;
+  }
+
+  if (
+    pressedBtn.classList.contains("point") &&
+    result.textContent.indexOf(".") !== -1
+  )
+    return;
+  if (
+    result.textContent === "0" &&
+    pressedBtn.classList.contains("number") &&
+    !pressedBtn.classList.contains("point")
+  )
+    result.textContent = "";
+  if (pressedBtn.classList.contains("number")) {
+    if (putToZero) {
+      result.textContent = "";
+      putToZero = false;
+    }
+    result.textContent = result.textContent + pressedBtn.value;
+  }
+
+  if (pressedBtn.value === "Clear") {
+    reset();
+  }
+
+  if (pressedBtn.classList.contains("aritmetic_operator")) {
+    const isZero = await Dapp_calculator_backend.see();
+    if (isZero === 0) add(parseFloat(result.textContent));
+    lastAritmeticOperator = pressedBtn.id;
+    justMadeIqual = false;
+    putToZero = true;
+  }
+
+  if (pressedBtn.classList.contains("iqual")) {
+    if (lastAritmeticOperator === "plus") add(parseFloat(result.textContent));
+    if (lastAritmeticOperator === "minus") sub(parseFloat(result.textContent));
+    if (lastAritmeticOperator === "multiply")
+      mul(parseFloat(result.textContent));
+    if (lastAritmeticOperator === "div") div(parseFloat(result.textContent));
+    lastAritmeticOperator = "";
+    justMadeIqual = true;
+    putToZero = true;
+  }
+  adjustResultSize();
+});
+
+//
+
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+
+//   const authClient = await AuthClient.create();
+
+//   if (await authClient.isAuthenticated()) {
+//     console.log("Autenticado");
+//   } else {
+//     await authClient.login({
+//       identityProvider: "https://identity.ic0.app/#authorize",
+//       onSuccess: () => {
+//         console.log("acaba de logearse");
+//       },
+//     });
+//   }
+
+//   const button = e.target.querySelector("button");
+
+//   const name = document.getElementById("name").value.toString();
+
+//   button.setAttribute("disabled", true);
+
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await Dapp_calculator_backend.greet(name);
+
+//   button.removeAttribute("disabled");
+
+//   document.getElementById("greeting").innerText = greeting;
+
+//   return false;
+// });
